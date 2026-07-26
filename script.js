09705b9df61dfc821da2c335c0ebb969250f8a95
Net Work <54335602+netw20200@users.noreply.github.com>
2026-07-26 12:03:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -54,17 +54,11 @@ if (products.length === 0) {
         return;
     }
 result.innerHTML = `
-<div class="w3-card-4 w3-white w3-round-xxlarge w3-padding"
-style="max-width:700px;margin:30px auto;">
+<div class="card">
 
-<img src="https://raw.githubusercontent.com/netw20200/autoparts-search/main/images/${found.photo}"
-style="width:100%;max-height:320px;object-fit:contain;border-radius:15px;">
+<img src="https://raw.githubusercontent.com/netw20200/autoparts-search/main/images/${found.photo}" alt="Фото товара">
 
-<h2 style="color:#d32f2f;margin-top:20px;">
-${found.description}
-</h2>
-
-<hr>
+<h2>${found.description}</h2>
 
 <p><b>Код:</b> ${found.code}</p>
 
@@ -72,10 +66,8 @@ ${found.description}
 
 <p><b>Количество:</b> ${found.quantity}</p>
 
+<p><b>Цена:</b> ${found.price}</p>
 
-<p style="font-size:30px;color:#0b8f32;font-weight:bold;text-align:right;">
-Цена: ${found["Цена"]}
-</p>
 </div>
 `;
     
