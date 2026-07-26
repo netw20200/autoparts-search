bf5b5753bbf83e4db8d9e8e8b1eb2062d0a2dc6d
Net Work <54335602+netw20200@users.noreply.github.com>
2026-07-27 00:00:50 +0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,28 @@
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+const orderItems = document.getElementById("orderItems");
+const orderTotal = document.getElementById("orderTotal");
+
+let total = 0;
+
+cart.forEach(item => {
+
+    total += item.price * item.count;
+
+    orderItems.innerHTML += `
+<div class="w3-padding" style="border-bottom:1px solid #ddd;">
+
+<b>${item.description}</b><br>
+
+${item.count} × ${item.price} грн
+
+<span style="float:right;font-weight:bold;">
+${item.price * item.count} грн
+</span>
+
+</div>
+`;
+
+});
+
+orderTotal.innerText = total;
