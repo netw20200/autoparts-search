3c4343bef7826ad4b6e595f112c48f64bd4bf2c3
Net Work <54335602+netw20200@users.noreply.github.com>
2026-07-27 00:07:24 +0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -26,3 +26,45 @@ ${item.price * item.count} грн
 });
 
 orderTotal.innerText = total;
+
+function sendOrder(){
+
+const name = document.getElementById("customerName").value;
+const phone = document.getElementById("customerPhone").value;
+const email = document.getElementById("customerEmail").value;
+const comment = document.getElementById("customerComment").value;
+
+let orderText = "";
+
+cart.forEach(item=>{
+
+orderText += `${item.description} (${item.count} шт.) - ${item.price * item.count} грн\n`;
+
+});
+
+emailjs.send("service_c66yum5","template_c2jum1w",{
+
+name: name,
+phone: phone,
+email: email,
+comment: comment,
+order: orderText,
+total: total + " грн"
+
+}).then(function(){
+
+alert("Заказ успешно отправлен!");
+
+localStorage.removeItem("cart");
+
+window.location.href="index.html";
+
+},function(error){
+
+alert("Ошибка отправки!");
+
+console.log(error);
+
+});
+
+}
