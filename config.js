73f92f1f102be13c86ee8f317bce0ebeb86a2e4f
Net Work <54335602+netw20200@users.noreply.github.com>
2026-07-27 20:22:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,21 @@
+const CONFIG = {
 
+    SHOP_NAME: "AutoParts Search",
+
+    PHONE: "+380111111111",
+
+    EMAIL: "shop@gmail.com",
+
+    LOGO: "images/logo.png",
+
+    API_URL: "https://script.google.com/macros/s/AKfycbyA_BB-oQcnV2VqmojnFS1SsRR4cjwZv20btw3_KXl__-QFfas0Om2tiz07bxtxEuAJrw/exec",
+
+    EMAILJS_PUBLIC_KEY: "NZE-dG86G7WYYz874",
+
+    EMAILJS_SERVICE: "service_c66yum5",
+
+    EMAILJS_TEMPLATE_ORDER: "template_c2jum1w",
+
+    EMAILJS_TEMPLATE_VIN: "template_qcggelu"
+
+};
